e5fd21f4d46c6845b3d2c943da9a4c023ecc3c44
Jose Nunez <jose.nunez@mhp.com>
2020-08-27 22:53:37 +0200

    update gitignore file and add test for search label

--- src/test/kotlin/ExampleTest.kt
+++ src/test/kotlin/ExampleTest.kt
@@ -1,7 +1,10 @@
 import it.skrape.core.htmlDocument
 import it.skrape.expect
 import it.skrape.matchers.toBe
+import it.skrape.matchers.toBePresent
+import it.skrape.selects.html5.a
 import it.skrape.selects.html5.h1
+import it.skrape.selects.html5.label
 import it.skrape.skrape
 import org.junit.jupiter.api.Test
 
@@ -17,6 +20,23 @@ class ExampleTest {
                         withClass = "i-header-claim-primary"
                         findFirst {
                             text toBe "Deutschlands großer Preisvergleich"
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `check text in searchlabel is displayed `() {
+        skrape {
+            url = "https://www.idealo.de/"
+            expect {
+                htmlDocument {
+                    label {
+                        withClass = "i-search-label"
+                        findFirst {
+                            text toBe "Ich suche ..."
                         }
                     }
                 }
